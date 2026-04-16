23fcb3bfc6e1c77e500956301b804fd6685ea77f
GP1323 <gnanaprasuna1315@gmail.com>
2026-04-16 11:14:54 +0530

    GP Added UC16

--- App/src/TrainApp.java
+++ App/src/TrainApp.java
@@ -1,50 +1,46 @@
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String message) {
-        super(message);
-    }
-}
-
-class GoodsBogie {
-    String shape;
-    String cargo;
-
-    GoodsBogie(String shape) {
-        this.shape = shape;
-    }
-
-    void assignCargo(String cargo) {
-        try {
-            System.out.println("Assigning cargo: " + cargo + " to " + shape + " bogie");
-
-            // Safety rule: Rectangular cannot carry Petroleum
-            if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
-                throw new CargoSafetyException("Unsafe cargo assignment: Petroleum not allowed in Rectangular bogie");
-            }
-
-            this.cargo = cargo;
-            System.out.println("Cargo assigned successfully");
-
-        } catch (CargoSafetyException e) {
-            System.out.println("ERROR: " + e.getMessage());
-
-        } finally {
-            System.out.println("Finalizing assignment for " + shape + " bogie\n");
-        }
-    }
-}
-
-public class TrainAppUC15 {
+public class TrainAppUC16 {
     public static void main(String[] args) {
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        b1.assignCargo("Petroleum"); // safe
+        // Passenger bogie capacities
+        int[] capacities = {72, 56, 24, 70, 60};
 
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
-        b2.assignCargo("Petroleum"); // unsafe
+        System.out.println("Before Sorting:");
+        printArray(capacities);
 
-        GoodsBogie b3 = new GoodsBogie("Rectangular");
-        b3.assignCargo("Coal"); // safe
+        // Bubble Sort Logic
+        int n = capacities.length;
 
-        System.out.println("System continues running safely...");
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+
+                    // swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // Optimization: stop if already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+
+        System.out.println("\nAfter Sorting:");
+        printArray(capacities);
+    }
+
+    static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
