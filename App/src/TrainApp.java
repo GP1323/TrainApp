968b2a6f99cdc3a6fd458d2b51ff4ba58c419598
GP1323 <gnanaprasuna1315@gmail.com>
2026-04-16 11:17:20 +0530

    GP Added UC17

--- App/src/TrainApp.java
+++ App/src/TrainApp.java
@@ -1,46 +1,24 @@
-public class TrainAppUC16 {
+import java.util.Arrays;
+
+public class TrainAppUC17 {
     public static void main(String[] args) {
 
-        // Passenger bogie capacities
-        int[] capacities = {72, 56, 24, 70, 60};
+        // Bogie type names
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
 
         System.out.println("Before Sorting:");
-        printArray(capacities);
+        System.out.println(Arrays.toString(bogieNames));
 
-        // Bubble Sort Logic
-        int n = capacities.length;
-
-        for (int i = 0; i < n - 1; i++) {
-
-            boolean swapped = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-
-                if (capacities[j] > capacities[j + 1]) {
-
-                    // swap
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-
-                    swapped = true;
-                }
-            }
-
-            // Optimization: stop if already sorted
-            if (!swapped) {
-                break;
-            }
-        }
+        // Built-in sorting (no manual logic)
+        Arrays.sort(bogieNames);
 
         System.out.println("\nAfter Sorting:");
-        printArray(capacities);
-    }
-
-    static void printArray(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
